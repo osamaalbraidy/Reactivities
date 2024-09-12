aa9f218ec15f165b206a5ce1e33e4767fa7b7a26
Osama Al Braidy <115343709+osamaalbraidy@users.noreply.github.com>
2024-09-12 10:34:34 +0300

    End Of Section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+import { Header, List } from 'semantic-ui-react';
+
+export interface Activity {
+  id: string;
+  date: string;
+  title: string;
+  description: string;
+  category: string;
+  city: string;
+  venue: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(res => {
+        setActivities(res.data);
+      })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header as={'h2'} icon={'users'} content='Reactivities' />
+      <List>
+        {
+          activities.map((activity, index) => (
+            <List.Item key={index}>
+              <Header as={'h2'} content={activity.title} />
+              <List>
+                <List.Item><strong>ID: </strong>{activity.id}</List.Item>
+                <List.Item><strong>Description: </strong>{activity.description}</List.Item>
+                <List.Item><strong>Category: </strong>{activity.category}</List.Item>
+                <List.Item><strong>City: </strong>{activity.city}</List.Item>
+                <List.Item><strong>Venue: </strong>{activity.venue}</List.Item>
+              </List>
+            </List.Item>
+          ))
+        }
+      </List>
     </>
   )
 }
